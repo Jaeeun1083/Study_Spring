b24dd086ba0b3a3c38aa219ed663ebf0feaca751
jaeeun <jjomang7602@naver.com>
2022-04-11 13:39:36 +0900

    feat: 유효성 체크를 위한 Validation API 사용

--- Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/exception/CustomResponseEntityExceptionHandler.java
+++ Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/exception/CustomResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.example.exception;
 
 import com.example.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -27,6 +29,12 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
                 new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),"Validation failed", ex.getBindingResult().toString());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
--- Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/user/UserController.java
+++ Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -31,7 +32,7 @@ public class UserController {
     }
 
     @PostMapping("/users") //Restful API를 설계할 때 복수형 설계 권장. 단일 객체만을 위한 URI이 아니라, USERS라는 데이터 리소스에 새로운 목록의 데이터를 추가하기 위한 API 이므로
-    public ResponseEntity<User> createUser(@RequestBody User user) { // ResponseEntity 사용 시 반환 데이터를 위한 Response Status Code를 포함해서 전달할 수 있기 때문에
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) { // ResponseEntity 사용 시 반환 데이터를 위한 Response Status Code를 포함해서 전달할 수 있기 때문에
         User saveUser = service.save(user);
 
         // ServletUriComponentsBuilder : 사용자 요청에 따른 작업을 처리한 다음, 결과 값을 토대로 관련 URI를 생성해 주는 역할
