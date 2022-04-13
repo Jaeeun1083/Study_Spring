972e0e606b8d9fc586d56620df8541a65b8945c5
jaeeun <jjomang7602@naver.com>
2022-04-13 17:29:14 +0900

    feat: REST API Documentation을 위한 Swagger 사용

--- Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/user/User.java
+++ Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/user/User.java
@@ -1,6 +1,8 @@
 package com.example.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,15 +15,24 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 // @JsonIgnoreProperties(value = ("password"))
-//@JsonFilter("UserInfo")
+// @JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체") // swagger 페이지의 user 객체 description 변경
 public class User {
 
     private Integer id;
+
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
+
     @Past
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요.")
     private Date joinDate;
+
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요.")
     private String password;
+
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요.")
     private String ssn;
 
 }
