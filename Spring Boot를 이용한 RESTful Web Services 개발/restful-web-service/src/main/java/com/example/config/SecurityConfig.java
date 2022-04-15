25c2a0da03e194ca9b79a2bc6d963cb3901890fc
jaeeun <jjomang7602@naver.com>
2022-04-15 15:22:21 +0900

    fix: h2 console 사용을 위한 권한 허가

--- Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/config/SecurityConfig.java
+++ Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -17,6 +18,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .withUser("Kenneth")
                     .password("{noop}test1234") // 인코딩 없이 처리하기 위한 {noop} 옵션
                     .roles("USER");
-        }
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
+    }
 
 }
