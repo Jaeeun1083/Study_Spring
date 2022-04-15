c86dbe587f6020be62805247cfefdac2607b2f4f
jaeeun <jjomang7602@naver.com>
2022-04-15 10:25:39 +0900

    feat: Spring Security를 이용한 인증 처리

---
+++ Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Configuration 클래스를 이용한 사용자 인증 처리
+ * */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("Kenneth")
+                    .password("{noop}test1234") // 인코딩 없이 처리하기 위한 {noop} 옵션
+                    .roles("USER");
+        }
+
+}
