a59171926d4520ef8b6d4e642c88428a5b9e8bd5
jaeeun <jjomang7602@naver.com>
2022-04-21 09:55:50 +0900

    feat: Data JPA를 사용한 User CRUD

--- Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/user/User.java
+++ Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/java/com/example/user/User.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -17,8 +20,11 @@ import java.util.Date;
 // @JsonIgnoreProperties(value = ("password"))
 // @JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체") // swagger 페이지의 user 객체 description 변경
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
---
+++ Spring Boot를 이용한 RESTful Web Services 개발/restful-web-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(90001, sysdate(), 'User1', 'test1', '701010-1111111');
+insert into user values(90002, sysdate(), 'User2', 'test2', '801010-2222222');
+insert into user values(90003, sysdate(), 'User3', 'test3', '901010-3333333');
